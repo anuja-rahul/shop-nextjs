264663d915a43bf80e6b74504653b064fccae862
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-05-08 00:59:47 +0530

    added product server action

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,8 +1,34 @@
+import { redirect } from "next/navigation";
+import prisma from "../lib/db/prisma";
+
+export const metadata = {
+  title: "Add Product - Nextore",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields !");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
+
 export default function AddProductPage() {
   return (
     <div>
       <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-      <form>
+      <form action={addProduct}>
         <input
           required
           name="name"
@@ -30,7 +56,9 @@ export default function AddProductPage() {
           placeholder="Price"
           className="input input-bordered mb-3 w-full"
         />
-        <button type="submit" className="btn btn-primary btn-block">Add Product</button>
+        <button type="submit" className="btn btn-primary btn-block">
+          Add Product
+        </button>
       </form>
     </div>
   );
