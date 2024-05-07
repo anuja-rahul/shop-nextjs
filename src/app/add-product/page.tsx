c57fa62cd484dc390b9894a4bfab45cac4c53acc
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-05-07 20:18:53 +0530

    added form

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,7 +1,37 @@
 export default function AddProductPage() {
   return (
     <div>
-      <h1 className="text-lg">Add Product</h1>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form>
+        <input
+          required
+          name="name"
+          type="text"
+          placeholder="Name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          type="number"
+          placeholder="Price"
+          className="input input-bordered mb-3 w-full"
+        />
+        <button type="submit" className="btn btn-primary btn-block">Add Product</button>
+      </form>
     </div>
   );
 }
