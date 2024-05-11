136e29566326dea2c270e9874ec574cbc87330ca
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-05-11 20:04:38 +0530

    added NavBar component

---
+++ src/app/Navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="bg-base-100">
+      <div className="navbar m-auto max-w-7xl flex-col gap-2 sm:flex-row"></div>
+    </div>
+  );
+}
